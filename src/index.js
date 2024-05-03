8fb5a1b89bb4c643e721960457b7cea623c0b69d
Igor Bezhinarov <igorbezhinarov@yandex.ru>
2024-05-03 18:06:46 +0700

    added the functionality of showing a random character

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
 import './style/style.scss';
 
-const marvelService = new MarvelService()
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
 
 root.render(
   <React.StrictMode>
